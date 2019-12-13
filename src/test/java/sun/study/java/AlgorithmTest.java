0539bead4ed3a52ced19c2e5269a8d013460430d
sunxiaobo7 <sunxiaobo@jd.com>
2019-12-13 12:03:10 +0800

    add test method

---
+++ src/test/java/sun/study/java/AlgorithmTest.java
@@ -0,0 +1,12 @@
+package sun.study.java;
+
+public class AlgorithmTest {
+
+    public static void main(String[] args) {
+        int[] arr = {12,4,62,5,28,45,94,924,73,75,23,8,43,73,46,765,98};
+
+
+    }
+
+
+}
